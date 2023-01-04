2a0acbad542e7d7ff4876f17d2736e12ffd3706d
Jeffrey Schneider <jeffrey.schneider@chattanoogastate.edu>
2023-01-04 05:24:17 -0500

    Update MainWindow.xaml.cs

--- LoanAmortization/LoanAmortization/MainWindow.xaml.cs
+++ LoanAmortization/LoanAmortization/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace LoanAmortization
     public partial class MainWindow : Window
     {
         DateTime localDate = DateTime.Now;
-        public static Borrower borrower = new Borrower();        
+        public static Borrower borrower = new Borrower();
         public static LoanInfo loanInfo = new LoanInfo();
         public static ObservableCollection<LoanPayments> theList = new ObservableCollection<LoanPayments>();
         Random rnd = new Random();
@@ -32,10 +32,10 @@ namespace LoanAmortization
             };
 
             lstState.ItemsSource = listStates;
-            lstState.SelectedIndex = lstState.Items.IndexOf("TN");           
+            lstState.SelectedIndex = lstState.Items.IndexOf("TN");
         }
 
-       
+
         private void mnuAbout_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Forms.MessageBox.Show("\nWPFLoanAmortization\n\nVersion 1.1.0");
@@ -48,11 +48,11 @@ namespace LoanAmortization
 
         private void ClearForm_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Forms.MessageBox.Show("Clear Form Click");
+            //System.Windows.Forms.MessageBox.Show("Clear Form Click");
 
             //loanInfo = null;
             borrower = null;
-            foreach(var ctl in containerText.Children)
+            foreach (var ctl in containerText.Children)
             {
                 if (ctl.GetType() == typeof(TextBox))
                     ((TextBox)ctl).Text = String.Empty;
@@ -99,43 +99,42 @@ namespace LoanAmortization
 
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
-            
             int loanNo = rnd.Next();
             double principal;
             int numberOfPayments;
             double annualRate;
 
-                if (!double.TryParse(txtAmount.Text, out  principal))
-                {
-                    System.Windows.Forms.MessageBox.Show("Error parsing amount");
-                    txtAmount.Focus();
-                }
-                if (!int.TryParse(txtDuration.Text, out  numberOfPayments))
-                {
-                    System.Windows.Forms.MessageBox.Show("Error parsing duration");
-                    txtDuration.Focus();
-                }
-                if (!double.TryParse(txtInterestRate.Text, out  annualRate))
-                {
-                    System.Windows.Forms.MessageBox.Show("Error parsing interest rate");
-                    txtInterestRate.Focus();
-                }
+            if (!double.TryParse(txtAmount.Text, out principal))
+            {
+                System.Windows.Forms.MessageBox.Show("Error parsing amount");
+                txtAmount.Focus();
+            }
+            if (!int.TryParse(txtDuration.Text, out numberOfPayments))
+            {
+                System.Windows.Forms.MessageBox.Show("Error parsing duration");
+                txtDuration.Focus();
+            }
+            if (!double.TryParse(txtInterestRate.Text, out annualRate))
+            {
+                System.Windows.Forms.MessageBox.Show("Error parsing interest rate");
+                txtInterestRate.Focus();
+            }
 
-                DateTime parsedDate = datePicker.SelectedDate.Value;
+            DateTime parsedDate = datePicker.SelectedDate.Value;
 
-                //If user entered whole number as interest rate, divide it by 100
-                if (annualRate >= 1)
-                {
-                    annualRate /= 100;
-                    txtInterestRate.Text = annualRate.ToString();
-                }
-                double monthlyPayment = calculateMonthlyPayment(principal, numberOfPayments, annualRate);
-                double interestPaid = 0.0;
-                double principalPaid = 0.0;
-                double newBalance = 0.0;
+            //If user entered whole number as interest rate, divide it by 100
+            if (annualRate >= 1)
+            {
+                annualRate /= 100;
+                txtInterestRate.Text = annualRate.ToString();
+            }
+            double monthlyPayment = calculateMonthlyPayment(principal, numberOfPayments, annualRate);
+            double interestPaid = 0.0;
+            double principalPaid = 0.0;
+            double newBalance = 0.0;
 
-                showPayment.Text = monthlyPayment.ToString();
-            
+            showPayment.Text = monthlyPayment.ToString();
+
             // Is this a new loan or an update of previous submission?
             if (loanInfo is null)
             {
@@ -148,84 +147,99 @@ namespace LoanAmortization
                 loanInfo.Duration = numberOfPayments;
                 loanInfo.InterestRate = annualRate;
                 loanInfo.BeginDate = parsedDate;
-                theList.Clear();                
+                theList.Clear();
             }
-            
-                showAmount.Text = loanInfo.Amount.ToString();
-                showDuration.Text = loanInfo.Duration.ToString();
-                showRate.Text = loanInfo.InterestRate.ToString();
-                showDate.Text = loanInfo.BeginDate.ToString("MM/dd/yyyy");
-                showEndDate.Text = loanInfo.EndDate.ToString("MM/dd/yyyy");
-                txtEndDate.Text = loanInfo.EndDate.ToString("MM/dd/yyyy");
 
-                for (int counter = 1; counter <= numberOfPayments; counter++)
-                {
-                    interestPaid = Math.Round((principal * (annualRate / 12)), 2, MidpointRounding.ToEven);
-                    principalPaid = Math.Round((monthlyPayment - interestPaid), 2, MidpointRounding.ToEven);
-                    newBalance = Math.Round((principal - principalPaid), 2, MidpointRounding.ToEven);
+            showAmount.Text = loanInfo.Amount.ToString();
+            showDuration.Text = loanInfo.Duration.ToString();
+            showRate.Text = loanInfo.InterestRate.ToString();
+            showDate.Text = loanInfo.BeginDate.ToString("MM/dd/yyyy");
+            showEndDate.Text = loanInfo.EndDate.ToString("MM/dd/yyyy");
+            txtEndDate.Text = loanInfo.EndDate.ToString("MM/dd/yyyy");
 
-                    theList.Add(new LoanPayments(loanNo, counter, principal, monthlyPayment, interestPaid, principalPaid, newBalance, parsedDate.AddMonths(counter - 1)));
+            for (int counter = 1; counter <= numberOfPayments; counter++)
+            {
+                interestPaid = Math.Round((principal * (annualRate / 12)), 2, MidpointRounding.ToEven);
+                principalPaid = Math.Round((monthlyPayment - interestPaid), 2, MidpointRounding.ToEven);
+                newBalance = Math.Round((principal - principalPaid), 2, MidpointRounding.ToEven);
 
-                    principal = newBalance;
-                }
-                dgSimple.ItemsSource = theList;
-            
-            
+                theList.Add(new LoanPayments(loanNo, counter, principal, monthlyPayment, interestPaid, principalPaid, newBalance, parsedDate.AddMonths(counter - 1)));
+
+                principal = newBalance;
+            }
+            dgSimple.ItemsSource = theList;
+
+
 
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Random rnd = new Random();
-            
+
         }
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (System.Windows.MessageBox.Show("Close Application?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
             {
                 e.Cancel = true;
-            }            
-            
+            }
         }
-
-       
 
         private void Font_Update_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Forms.FontDialog fontDialog = new System.Windows.Forms.FontDialog();
-            if (fontDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            fontDialog.MaxSize = 40;
+            fontDialog.MinSize = 8;
+
+            if (fontDialog.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
             {
-                FontFamilyConverter ffc = new FontFamilyConverter();
+                foreach (var ctl in containerText.Children)
+                {
+                    FontFamilyConverter ffc = new FontFamilyConverter();
+                    var selectedFont = fontDialog.Font;
+                    if (ctl.GetType() == typeof(TextBlock))
+                    {
+                        ((TextBlock)ctl).FontSize = selectedFont.Size;
+                        ((TextBlock)ctl).FontFamily = (FontFamily)ffc.ConvertFromString(fontDialog.Font.Name);
+                        ((TextBlock)ctl).FontWeight = FontWeights.Normal;
+                        ((TextBlock)ctl).FontStyle = FontStyles.Normal;
+                        if (fontDialog.Font.Bold)
+                            ((TextBlock)ctl).FontWeight = FontWeights.Bold;
+                        if (fontDialog.Font.Italic)
+                            ((TextBlock)ctl).FontStyle = FontStyles.Italic;
+                    }
 
-                this.txtName.FontSize = fontDialog.Font.Size;
-                this.lstState.FontSize = fontDialog.Font.Size;
-                this.txtName.FontFamily = (FontFamily)ffc.ConvertFromString(fontDialog.Font.Name);
-                this.lstState.FontFamily = (FontFamily)ffc.ConvertFromString(fontDialog.Font.Name);
+                    if (ctl.GetType() == typeof(TextBox)) 
+                    {
+                        ((TextBox)ctl).FontSize = selectedFont.Size;
+                        ((TextBox)ctl).FontFamily = (FontFamily)ffc.ConvertFromString(fontDialog.Font.Name);
+                        ((TextBox)ctl).FontWeight = FontWeights.Normal;
+                        ((TextBox)ctl).FontStyle = FontStyles.Normal;
+                        if (fontDialog.Font.Bold)
+                            ((TextBox)ctl).FontWeight = FontWeights.Bold;
+                        if (fontDialog.Font.Italic)
+                            ((TextBox)ctl).FontStyle = FontStyles.Italic;
+                    }
 
-                if (fontDialog.Font.Bold)
-                {
-                    txtName.FontWeight = FontWeights.Bold;
-                    lstState.FontWeight = FontWeights.Bold;
-                }
-                else
-                {
-                    txtName.FontWeight = FontWeights.Normal;
-                    lstState.FontWeight = FontWeights.Normal;
-                }
-
-                if (fontDialog.Font.Italic)
-                {
-                    txtName.FontStyle = FontStyles.Italic;
-                    lstState.FontStyle = FontStyles.Italic;
-                }
-                else
-                {
-                    txtName.FontStyle = FontStyles.Normal;
-                    lstState.FontStyle = FontStyles.Normal;
+                    if(ctl.GetType() == typeof(ListBox))
+                    {
+                        ((ListBox)ctl).FontSize = selectedFont.Size;
+                        ((ListBox)ctl).FontFamily = (FontFamily)ffc.ConvertFromString(fontDialog.Font.Name);
+                        ((ListBox)ctl).FontWeight = FontWeights.Normal;
+                        ((ListBox)ctl).FontStyle = FontStyles.Normal;
+                        if (fontDialog.Font.Bold)
+                            ((ListBox)ctl).FontWeight = FontWeights.Bold;
+                        if (fontDialog.Font.Italic)
+                            ((ListBox)ctl).FontStyle = FontStyles.Italic;
+                    }
                 }
             }
-
         }
+
+           
+
+   
 
         public static double calculateMonthlyPayment(double loanAmount, int term, double annualRate)
         {
